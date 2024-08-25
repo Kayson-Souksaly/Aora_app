1a79317c90cfa24b65cb54ec2edeba096547328f
Kayson-Souksaly <kayson.souksaly@gmail.com>
2024-08-24 21:04:34 -0400

    added a form field to the log in page
    features added:
    
    - added a use state field for email
    - change formField with title, value, handleChangeText, otherStyles, keyboardType

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,12 +2,19 @@ import { Text, View, Image, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import { useState } from 'react';
 
 const SignIn = () => {
+  const [form, setform] = useState({
+    email: '',
+    password: ''
+  })
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+        <View className="w-full justify-center h-full px-4 my-6">
           <Image 
             source={images.logo}
             resizeMode='conn'
@@ -17,6 +24,14 @@ const SignIn = () => {
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
           </Text>
+
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setform({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>

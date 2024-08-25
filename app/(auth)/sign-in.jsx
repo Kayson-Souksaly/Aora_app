a0438d3f22d80e16b00eee2364f20f187fd63764
Kayson-Souksaly <kayson.souksaly@gmail.com>
2024-08-24 20:55:02 -0400

    Changing the layout of the sign in screen
    features added to the sign-in screen:
    
    - added image library
    - changes view to a safeAreaView
    - made it scrollable
    - added the Aora logo
    - added some text with styling

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image 
+            source={images.logo}
+            resizeMode='conn'
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 

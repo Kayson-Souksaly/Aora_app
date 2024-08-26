b035c23cc85e14025b484c0ed74df2f4bd6c930c
Kayson-Souksaly <kayson.souksaly@gmail.com>
2024-08-26 19:42:57 -0400

    change the sign in screen with the same code from sign up

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, ScrollView } from 'react-native'
+import { Text, View, Image, ScrollView, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants';
@@ -6,7 +6,8 @@ import FormField from '../../components/FormField';
 import { useState } from 'react';
 
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { signIn } from '../../lib/appwrite';
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -16,8 +17,24 @@ const SignIn = () => {
   
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if( !form.email || !form.password){
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
 
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password)
+
+      // set it to global state... 
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (

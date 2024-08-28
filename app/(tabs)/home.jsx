90988964ec3c3c5fd22469cc6f0faef7f04dc9f4
Kayson-Souksaly <kayson.souksaly@gmail.com>
2024-08-27 23:02:18 -0400

    adding a trending section in the app
    new features:
    
    - flatlist that received posts data and display it
    - added a trending.jsx component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
@@ -44,6 +45,10 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
+
+              <Trending 
+                posts={[{ id: 1}, { id: 2}, { id: 3}] ?? []}
+              />
             </View>
           </View>
         )}

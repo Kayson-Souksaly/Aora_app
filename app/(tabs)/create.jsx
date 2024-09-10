cb0b094554b8072d1ac77af837547a35d1f47361
Kayson-Souksaly <kayson.souksaly@gmail.com>
2024-09-09 22:39:22 -0400

    Tutorial is finished
    New features:
    
    - utilizing the expo image picker to upload user's images and videos to the app
    - if any of the boxes are not filled, alert the user to fill them before uploading
    - logic to store uploads to the database
    - reusing components like FormField and CustomButton

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,23 +1,72 @@
-import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
-import { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Video, ResizeMode } from 'expo-av' 
-import { icons } from '../../constants'
+import { View, Text, ScrollView, TouchableOpacity, Image, Alert } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Video, ResizeMode } from 'expo-av';
+import * as ImagePicker from 'expo-image-picker';
+
+import { icons, images } from '../../constants';
+import { router } from 'expo-router';
+import { createVideo } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 
 const Create = () => {
-  const [uploading, setUploading] = useState(false)
+  const { user } = useGlobalContext();
+  const [uploading, setUploading] = useState(false);
   const [form, setForm] = useState({
     title: '',
     video: null,
     thumbnail: null,
     prompt: ''
-  })
+  });
 
-  const submit = () => {
+  const openPicker = async (selectType) => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    if(!result.canceled) {
+      if(selectType === 'image') {
+        setForm({ ...form, thumbnail: result.assets[0]})
+      }
+
+      if(selectType === 'video') {
+        setForm({ ...form, video: result.assets[0]})
+      }
+    }
+  }
+
+  const submit = async () => {
+    if(!form.prompt || !form.title || !form.thumbnail || !form.video) {
+      return Alert.alert('Please fill in all the fields')
+    }
+
+    setUploading(true)
+
+    try {
+      await createVideo({
+        ...form, userId: user.$id
+      })
+
+      Alert.alert('Success', 'Post uploaded successfully')
+
+      router.push('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setForm({
+        title: '',
+        video: null,
+        thumbnail: null,
+        prompt: ''
+      })
+
+      setUploading(false);
+    }
   }
 
   return (
@@ -40,14 +89,14 @@ const Create = () => {
             Upload Video
           </Text>
 
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => openPicker('video')}
+          >
             {form.video ? (
               <Video 
                 source={{ uri: form.video.uri }}
                 className="w-full h-64 rounded-2xl"
-                useNativeControls
                 resizeMode={ResizeMode.COVER}
-                isLooping
               />
             ) : 
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
@@ -68,7 +117,9 @@ const Create = () => {
             Thumbnail Image
           </Text>
 
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => openPicker('image')}
+          >
             {form.thumbnail ? (
               <Image 
                 source={{ uri: form.thumbnail.uri }}
@@ -100,7 +151,7 @@ const Create = () => {
         />
 
         <CustomButton 
-          title="Submit"
+          title="Submit & publish"
           handlePress={submit}
           containerStyles="mt-7"
           isLoading={uploading}
